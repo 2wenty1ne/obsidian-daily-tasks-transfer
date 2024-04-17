838e9ad8e61f184fbeac1f4e1a92d8bc7cb66021
2wenty1ne <sirdarkvic@gmail.com>
2024-04-18 00:53:59 +0200

    Ribbon icon and reload added
    Added two ribbon icons. One two quickly use the reload function, one to display a button to switch to the last and next daily note.

--- main.ts
+++ main.ts
@@ -1,14 +1,66 @@
-import { Plugin } from 'obsidian'
+import { Editor, MarkdownView, Menu, Notice, Plugin } from 'obsidian'
+import { ExampleSettingTab } from "./settings";
+
+interface DailyTransferPluginSettings {
+	dateFormat: string;
+}
+
+const DEFAULT_SETTINGS: Partial<DailyTransferPluginSettings> = {
+	dateFormat: "YYYY-MM-DD",
+};
 
 export default class DailyTransferPlugin extends Plugin {
-	onload(): void {
-		const { vault, moment } = this.app
-		console.log(`Vault type: ${typeof vault}`)
-		console.log(`Vault content: ${vault}`)
-		console.log('Amogus')
+	settings: DailyTransferPluginSettings;
+
+	async onload(): void {
+		console.log('Loading DailyTransferPlugin');
+		await this.loadSettings();
+		const { vault } = this.app
+		
+		this.reloadRibbonButton()
+		this.getTodaysDailyNote()
+		this.quickAccessDailyRibbonButton()
 	}
 
-	function getTodaysDailyNote(): void{
-		
+	unload(): void {
+		console.log('Unloading DailyTransferPlugin');
+	}
+
+	getTodaysDailyNote(): void{
+		console.log("in func")
+	}
+
+	quickAccessDailyRibbonButton(): void{
+		const quickAccessButton = this.addRibbonIcon('calendar-range', 'Dailies', (event) => {
+			const menu = new Menu();
+			new Notice("today")
+
+			menu.addItem((item) =>
+				item
+					.setTitle("Tomorrow")
+					.setIcon("step-forward")
+					.onClick(() => {
+						new Notice("tomorrow");
+					})
+			);
+			menu.addSeparator();
+			menu.addItem((item) =>
+				item
+					.setTitle("Previous")
+					.setIcon("step-back")
+					.onClick(() => {
+						new Notice("previous");
+					})
+			);
+			menu.showAtPosition({ x: 200, y: 200 })
+			menu.showAtMouseEvent(event);
+			// menu.showAtMouseEvent()
+		})
+	} 
+
+	reloadRibbonButton(): void{
+		const reloadButton = this.addRibbonIcon('refresh-cw', 'Reload', () => {
+			this.app.commands.executeCommandById('app:reload');
+		});
 	}
 }
